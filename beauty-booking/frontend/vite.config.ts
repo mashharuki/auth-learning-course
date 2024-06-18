c088f0cd5d36fa056a9f7106e5e33490154b4447
kentny <kentaro.terasaki@gmail.com>
2024-06-18 19:52:56 +0900

    Setup docker files.

--- beauty-booking/frontend/vite.config.ts
+++ beauty-booking/frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true,
+    open: false,
     port: 5173,
     strictPort: true,
     proxy: {
